f3b91c626232270eae570e3f399c9e226ea0ab10
Victor Canto <iamvictorcanto@gmail.com>
2024-05-11 21:49:10 -0300

    test: ensure the Api and Checkout works properly

---
+++ test/api.test.ts
@@ -0,0 +1,175 @@
+import axios from "axios";
+axios.defaults.validateStatus = () => true;
+
+describe("API", () => {
+	test("Nao deve fazer um pedido com cpf invalido", async () => {
+		const input = {
+			cpf: "978.375.349-00",
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(422);
+		const output = response.data;
+		expect(output.message).toBe("Invalid cpf");
+	});
+
+	test("Deve fazer um pedido com 3 produtos", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 },
+			],
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(200);
+		const output = response.data;
+		expect(output.total).toBe(6350);
+	});
+
+	test("Nao deve fazer um pedido com produto que nao existe", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [{ idProduct: 4, quantity: 3 }],
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(422);
+		const output = response.data;
+		expect(output.message).toBe("Product not found");
+	});
+
+	test("Deve fazer um pedido com 3 produtos com cupom de desconto", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 },
+			],
+			coupon: "VALE20",
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(200);
+		const output = response.data;
+		expect(output.total).toBe(5132);
+	});
+
+	test("Nao deve aplicar o desconto se o cupom for invalido", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 },
+			],
+			coupon: "VALE100",
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(200);
+		const output = response.data;
+		expect(output.total).toBe(6350);
+	});
+
+	test("Deve fazer um pedido com 3 produtos com cupom expirado", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 },
+			],
+			coupon: "VALE50_EXPIRED",
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(200);
+		const output = response.data;
+		expect(output.total).toBe(6350);
+	});
+
+	test("Deve fazer um pedido com quantidade negativa", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [{ idProduct: 1, quantity: -3 }],
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(422);
+		const output = response.data;
+		expect(output.message).toBe("Quantity must be positive");
+	});
+
+	test("Deve fazer um pedido com quantidade negativa", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 1, quantity: 1 },
+			],
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(422);
+		const output = response.data;
+		expect(output.message).toBe("Duplicated product in the same order");
+	});
+
+	test("Deve fazer um pedido calculando o frete", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [{ idProduct: 1, quantity: 1 }],
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(200);
+		const output = response.data;
+		expect(output.total).toBe(1030);
+	});
+
+	test("Deve retornar o preco minimo de frete caso ele seja inferior ao valor calculado", async () => {
+		const input = {
+			cpf: "454.508.362-52",
+			items: [{ idProduct: 3, quantity: 1 }],
+		};
+
+		const response = await axios.post(
+			"http://localhost:3333/checkout",
+			input
+		);
+		expect(response.status).toBe(200);
+		const output = response.data;
+		expect(output.total).toBe(40);
+	});
+});
---
+++ test/checkout.test.ts
@@ -0,0 +1,103 @@
+import Checkout from "../src/checkout";
+import CouponData from "../src/coupon-data";
+import { CouponModel } from "../src/coupon-model";
+import ProductData from "../src/product-data";
+import { ProductModel } from "../src/product-model";
+
+const mockCouponDataDb = (): CouponData => {
+	class CouponDataDbStub implements CouponData {
+		async getCoupon(code: string): Promise<CouponModel> {
+			const coupons: { [code: string]: CouponModel } = {
+				VALE10: {
+					code: "VALE10",
+					percentage: 10,
+					expireDate: new Date("2025-01-01"),
+				},
+				VALE20: {
+					code: "VALE20",
+					percentage: 20,
+					expireDate: new Date("2025-01-01"),
+				},
+				VALE50_EXPIRED: {
+					code: "VALE50_EXPIRED",
+					percentage: 50,
+					expireDate: new Date("2024-01-01"),
+				},
+			};
+			return Promise.resolve(coupons[code]);
+		}
+	}
+	return new CouponDataDbStub();
+};
+
+const mockProductDataDb = (): ProductData => {
+	class ProductDataDbStub implements ProductData {
+		async getProduct(idProduct: number): Promise<ProductModel> {
+			const products: { [idProduct: number]: ProductModel } = {
+				1: {
+					idProduct: 1,
+					description: "A",
+					price: 1000,
+					width: 100,
+					height: 30,
+					length: 10,
+					weight: 3,
+				},
+				2: {
+					idProduct: 2,
+					description: "B",
+					price: 5000,
+					width: 50,
+					height: 50,
+					length: 50,
+					weight: 22,
+				},
+				3: {
+					idProduct: 3,
+					description: "C",
+					price: 30,
+					width: 10,
+					height: 10,
+					length: 10,
+					weight: 0.9,
+				},
+			};
+			return Promise.resolve(products[idProduct]);
+		}
+	}
+	return new ProductDataDbStub();
+};
+
+type SutTypes = {
+	couponDataStub: CouponData;
+	productDataStub: ProductData;
+	sut: Checkout;
+};
+
+const makeSut = (): SutTypes => {
+	const productDataStub = mockProductDataDb();
+	const couponDataStub = mockCouponDataDb();
+	const sut = new Checkout(productDataStub, couponDataStub);
+	return {
+		couponDataStub,
+		productDataStub,
+		sut,
+	};
+};
+
+describe("Checkout", () => {
+	test("Deve fazer um pedido com 3 produtos", async () => {
+		const { sut } = makeSut();
+		const input = {
+			cpf: "454.508.362-52",
+			items: [
+				{ idProduct: 1, quantity: 1 },
+				{ idProduct: 2, quantity: 1 },
+				{ idProduct: 3, quantity: 3 },
+			],
+		};
+
+		const output = await sut.execute(input);
+		expect(output.total).toBe(6350);
+	});
+});
