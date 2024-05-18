6fbbda424d9466ea59354e80f71c01f28b1148f0
Victor Canto <iamvictorcanto@gmail.com>
2024-05-18 04:05:24 -0300

    feat: add order and item tables

--- create.sql
+++ create.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS store.product;
 DROP TABLE IF EXISTS store.coupon;
+DROP TABLE IF EXISTS store.item;
+DROP TABLE IF EXISTS store.product;
+DROP TABLE IF EXISTS store.order;
 CREATE SCHEMA IF NOT EXISTS store;
 CREATE TABLE IF NOT EXISTS store.product (
     id_product SERIAL PRIMARY KEY,
@@ -33,3 +35,21 @@ INSERT INTO store.coupon (code, percentage, expire_date)
 VALUES ('VALE10', 10, '2025-01-01T00:00:00'),
     ('VALE20', 20, '2025-01-01T00:00:00'),
     ('VALE50_EXPIRED', 50, '2024-01-01T00:00:00');
+CREATE TABLE IF NOT EXISTS store.order (
+    id_order SERIAL PRIMARY KEY,
+    coupon_code TEXT,
+    coupon_percentage NUMERIC,
+    code TEXT,
+    cpf TEXT,
+    issue_date TIMESTAMP,
+    freight NUMERIC,
+    total NUMERIC,
+    sequence INTEGER
+);
+CREATE TABLE IF NOT EXISTS store.item (
+    id_order INTEGER REFERENCES store.order(id_order),
+    id_product INTEGER REFERENCES store.product(id_product),
+    price NUMERIC,
+    quantity INTEGER,
+    PRIMARY KEY (id_order, id_product)
+);
