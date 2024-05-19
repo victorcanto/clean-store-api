16efc14352f7c9b8f722288f0b9a38ebe65c44b5
Victor Canto <iamvictorcanto@gmail.com>
2024-05-19 03:03:42 -0300

    test: ensure the validity of coupon and return discount amount

---
+++ test/coupon.test.ts
@@ -0,0 +1,26 @@
+import Coupon from "../src/coupon";
+
+describe("Coupon", () => {
+	test("Deve testar se o cupom esta valido", () => {
+		const coupon = new Coupon("VALE20", 20, new Date());
+		expect(coupon.isExpired()).toBeFalsy();
+	});
+
+	test("Deve testar se o cupom esta expirado", () => {
+		const coupon = new Coupon(
+			"VALE20",
+			20,
+			new Date("2024-01-01T10:00:00")
+		);
+		expect(coupon.isExpired()).toBeTruthy();
+	});
+
+	test("Deve testar se o valor do desconto esta correto", () => {
+		const coupon = new Coupon(
+			"VALE50",
+			50,
+			new Date("2024-01-01T10:00:00")
+		);
+		expect(coupon.getDiscount(1000)).toBe(500);
+	});
+});
---
+++ test/validate-coupon.test.ts
@@ -0,0 +1,38 @@
+import CouponData from "../src/coupon-data";
+import ValidateCoupon from "../src/validate-coupon";
+import { fakeCouponDataDb } from "./helpers/fake";
+
+type SutTypes = {
+	couponDataStub: CouponData;
+	sut: ValidateCoupon;
+};
+
+const makeSut = (): SutTypes => {
+	const couponDataStub = fakeCouponDataDb();
+	const sut = new ValidateCoupon(couponDataStub);
+	return {
+		couponDataStub,
+		sut,
+	};
+};
+
+describe("Validate Coupon", () => {
+	test("Deve chamar o CouponDataDb para validar o cupom de desconto", async () => {
+		const { sut, couponDataStub } = makeSut();
+		const couponDataDbSpy = jest.spyOn(couponDataStub, "getCoupon");
+		await sut.execute("VALE20", 1000);
+		expect(couponDataDbSpy).toHaveBeenCalledWith("VALE20");
+	});
+
+	test("Deve validar um cupom de desconto", async () => {
+		const { sut } = makeSut();
+		const output = await sut.execute("VALE20", 1000);
+		expect(output.isExpired).toBeFalsy();
+	});
+
+	test("Deve retornar o valor de desconto", async () => {
+		const { sut } = makeSut();
+		const output = await sut.execute("VALE20", 1000);
+		expect(output.discount).toBe(200);
+	});
+});
