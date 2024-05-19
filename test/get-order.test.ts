4542ff40204b9682b9fa8d2c5a5e2c782e474934
Victor Canto <iamvictorcanto@gmail.com>
2024-05-19 00:46:00 -0300

    test: ensure that the request is consulted by the CPF

---
+++ test/get-order.test.ts
@@ -0,0 +1,42 @@
+import GetOrderByCPf from "../src/get-order-by-cpf";
+import OrderDataDb from "../src/order-data-db";
+import {
+	fakeCheckout,
+	fakeCheckoutInput,
+	fakeOrderDataDb,
+} from "./helpers/fake";
+
+type SutTypes = {
+	orderDataStub: OrderDataDb;
+	sut: GetOrderByCPf;
+};
+
+const makeSut = (): SutTypes => {
+	const orderDataStub = fakeOrderDataDb();
+	const sut = new GetOrderByCPf(orderDataStub);
+	return {
+		orderDataStub,
+		sut,
+	};
+};
+
+describe("Get Order", () => {
+	test("Deve chamar o OrderDataDb para consultar o pedido pelo CPF", async () => {
+		const checkout = fakeCheckout();
+		const input = fakeCheckoutInput();
+		const { sut, orderDataStub } = makeSut();
+		const orderDataDbSpy = jest.spyOn(orderDataStub, "getByCpf");
+		await checkout.execute(input);
+		await sut.execute(input.cpf);
+		expect(orderDataDbSpy).toHaveBeenCalledWith(input.cpf);
+	});
+
+	test("Deve retornar o total do pedido ao consultar o pedido pelo CPF", async () => {
+		const checkout = fakeCheckout();
+		const input = fakeCheckoutInput();
+		const { sut } = makeSut();
+		await checkout.execute(input);
+		const output = await sut.execute(input.cpf);
+		expect(output.total).toBe(6350);
+	});
+});
