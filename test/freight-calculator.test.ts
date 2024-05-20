f43212e290246e9aa33e8761b670c7a3ef9a0d32
Victor Canto <iamvictorcanto@gmail.com>
2024-05-20 01:56:49 -0300

    test: ensure that volume and density is calculated in the product entity

--- test/freight-calculator.test.ts
+++ test/freight-calculator.test.ts
@@ -1,23 +1,18 @@
 import FreightCalculator from "../src/domain/entities/freight-calculator";
+import Product from "../src/domain/entities/product";
 
 describe("Freight Calculator", () => {
 	test("Deve calcular o frete para um produto", () => {
-		const output = FreightCalculator.calculate({
-			width: 100,
-			height: 30,
-			length: 10,
-			weight: 3,
-		});
+		const output = FreightCalculator.calculate(
+			new Product(1, "A", 1000, 100, 30, 10, 3, "BRL")
+		);
 		expect(output).toBe(30);
 	});
 
 	test("Deve retornar o valor minimo de frete", () => {
-		const output = FreightCalculator.calculate({
-			width: 10,
-			height: 10,
-			length: 10,
-			weight: 0.9,
-		});
+		const output = FreightCalculator.calculate(
+			new Product(1, "A", 10, 10, 10, 10, 0.9, "BRL")
+		);
 		expect(output).toBe(10);
 	});
 });
---
+++ test/product.test.ts
@@ -0,0 +1,18 @@
+import Product from "../src/domain/entities/product";
+
+describe("Product", () => {
+	test("Deve criar um produto", () => {
+		const product = new Product(1, "A", 1000, 100, 30, 10, 3, "BRL");
+		expect(product).toBeTruthy();
+	});
+
+	test("Deve calcular o volume do produto", () => {
+		const product = new Product(1, "A", 1000, 100, 30, 10, 3, "BRL");
+		expect(product.getVolume()).toBe(0.03);
+	});
+
+	test("Deve calcular a densidade do produto", () => {
+		const product = new Product(1, "A", 1000, 100, 30, 10, 3, "BRL");
+		expect(product.getDensity()).toBe(100);
+	});
+});
