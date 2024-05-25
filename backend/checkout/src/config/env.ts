b993f94e4635b4e7243cb6c7a447b22e0d08cff9
Victor Canto <iamvictorcanto@gmail.com>
2024-05-25 00:51:21 -0300

    refactor: renane connection string

--- backend/checkout/src/config/env.ts
+++ backend/checkout/src/config/env.ts
@@ -2,5 +2,5 @@ export default {
 	port: process.env.POSTGRES_URL ?? 3333,
 	postgreUrl:
 		process.env.POSTGRES_URL ??
-		"postgresql://postgres:mypgdbpass@localhost:5432/clean-code-arch",
+		"postgresql://postgres:mypgdbpass@localhost:5432/cleanstore",
 };
